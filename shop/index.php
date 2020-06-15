c5e8a46536640e5a06ae98a3de83635eb3fb9513
DreadKnight <dk.vali@gmail.com>
2020-06-15 07:27:07 +0300

    big website revamp, fully dropped PHP dependency

--- shop/index.php
+++
@@ -1,8 +0,0 @@
-<?php $page_title = "Freezing Moon - Shop";
-require_once("../config.php");
-include("../header.php"); ?>
-<div class="segment center"><img src="coming_soon.jpg" class="rounded"></div>
-<div class="segment center">Check back in a few months for some awesome merchandise. You won't be disappointed!</div>
-<div class="segment">
-<?php include("../disqus.php"); ?>
-</div></body></html>
